f339fe6e5391521d911924e2f9a2ca8dcc0abc36
sam14032 <samuel-bisson@hotmail.com>
2018-08-20 19:22:39 -0400

    Added main scene

---
+++ Assets/Script/Playmode/Game/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LoadGame();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private static bool IsSceneLoaded(string name)
+    {
+        for (var i = 0; i < SceneManager.sceneCount; i++)
+            if (SceneManager.GetSceneAt(i).name == name) return true;
+
+        return false;
+    }
+
+    private static void LoadGame()
+    {
+        if (!IsSceneLoaded(Scenes.Game))
+        {
+            SceneManager.LoadSceneAsync(Scenes.Game, LoadSceneMode.Additive);
+        }
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(Scenes.Game));
+    }
+}
---
+++ Assets/Script/Playmode/Game/Scemes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Scenes
+{
+    public const string Main = "Main";
+    public const string Game = "Game";
+}
+
