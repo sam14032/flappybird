a642b5b62e4e490d7bcaba7c6e0ba93b01c44900
sam14032 <samuel-bisson@hotmail.com>
2018-08-20 19:04:05 -0400

    added player component to scene, added Assets

---
+++ Assets/Script/Playmode/Player/TranslateMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    [SerializeField]
+    private float speed = 5;
+    public void Move(Vector3 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
