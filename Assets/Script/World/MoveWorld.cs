5c2db50ab05800d1e368c4de691368687c48b7a0
sam14032 <samuel-bisson@hotmail.com>
2018-08-22 16:26:37 -0400

    All done, only missing music, pouf and bug fix

--- Assets/Script/World/MoveWorld.cs
+++ Assets/Script/World/MoveWorld.cs
@@ -1,41 +1,122 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MoveWorld : MonoBehaviour
 {
-	private SpriteRenderer[] backgroundIMG;
-	private SpriteRenderer[] foregroundIMG;
-	private int maxGameWidth;
-	private float halfIMGsize;
+	[SerializeField] private GameObject prefab;
+	[SerializeField] private float speed = 0.003f;
+	private SpriteRenderer backgroundIMG;
+	private GameObject[] worldObjectList;
+	private float maxGameWidth;
+	private float imgSize;
+	private Camera cam;
+	private float width;
+	private int numberOfPrefab;
+	private float height;
+	float highestYPosition;
+	
 	private void Awake()
 	{
-		backgroundIMG = GameObject.Find("Back").GetComponentsInChildren<SpriteRenderer>();
-		foregroundIMG = GameObject.Find("Front").GetComponentsInChildren<SpriteRenderer>();
-		maxGameWidth = Camera.main.pixelWidth;
-		halfIMGsize = backgroundIMG[0].size.x / 2;
+		backgroundIMG = GameObject.Find("Background").GetComponent<SpriteRenderer>();
+		cam = Camera.main;
+		height = cam.orthographicSize * 2f;
+		width = height * cam.aspect;
+		maxGameWidth = width;
+		transform.position = Vector3.right*-maxGameWidth/2f;
+		imgSize = backgroundIMG.size.x;
+		numberOfPrefab = (int)(maxGameWidth / imgSize)+3;
+		worldObjectList = new GameObject[numberOfPrefab];
+		CreatePrefab();
+		PositionObject();
+		highestYPosition = worldObjectList[numberOfPrefab - 1].transform.position.x;
 	}
 
 	// Use this for initialization
 	void Start () {
-		DisplayIMG();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (CheckIfCamSizeChanged())
+		{
+			transform.position = Vector3.right*-maxGameWidth/2f;
+			CreatePrefab();
+			PositionObject();
+		}
+		MovePrefab();
 	}
 
-	private void DisplayIMG()
+	private void ChangeNumberOfPrefab()
 	{
-		for (int i = 0; i < backgroundIMG.Length; ++i)
+		imgSize = backgroundIMG.size.x;
+		numberOfPrefab = (int)(maxGameWidth / imgSize)+3;
+	}
+	private void CreatePrefab()
+	{
+		if (worldObjectList[0] != null)
 		{
-			backgroundIMG[i].transform.position = new Vector3(maxGameWidth - i * 2 * halfIMGsize,
-				backgroundIMG[i].transform.position.y,
-				backgroundIMG[i].transform.position.z);
-			
-			foregroundIMG[i].transform.position = new Vector3(maxGameWidth - i * 2 * halfIMGsize,
-				foregroundIMG[i].transform.position.y,
-				foregroundIMG[i].transform.position.z);
+			DeleteOldPrefab();
+			ChangeNumberOfPrefab();
+			worldObjectList = new GameObject[numberOfPrefab];
+		}
+		for (int i = 0; i < numberOfPrefab; ++i)
+		{
+			worldObjectList[i] = Instantiate(prefab, transform.position, Quaternion.identity);
+		}
+	}
+
+	private void PositionObject()
+	{
+		for (int i = 0; i < numberOfPrefab; ++i)
+		{
+			worldObjectList[i].transform.position = transform.position + new Vector3(0.9f * i * imgSize,0,0);
+		}
+	}
+
+	bool CheckIfCamSizeChanged()
+	{
+		cam = Camera.main;
+		height = cam.orthographicSize * 2f;
+		float currentCamSize = height * cam.aspect;
+		if (currentCamSize != width)
+		{
+			width = currentCamSize;
+			maxGameWidth = width;
+			return true;
+		}
+		return false;
+	}
+
+	void DeleteOldPrefab()
+	{
+		for (int i = 0; i < numberOfPrefab; ++i)
+		{
+			worldObjectList[i].SetActive(false);
+		}
+	}
+	private void MovePrefab()
+	{
+		for (int i = 0; i < numberOfPrefab; ++i)
+		{
+			worldObjectList[i].transform.position = worldObjectList[i].transform.position - new Vector3(speed,0,0)*Time.deltaTime;
+		}
+		MovePrefabBack();
+	}
+
+	void MovePrefabBack()
+	{
+		for (int i = 0; i < numberOfPrefab; ++i)
+		{
+			if (worldObjectList[i].transform.position.x + imgSize / 2 < -(maxGameWidth / 2 -0.5))
+			{
+				worldObjectList[i].transform.position =
+					new Vector3(highestYPosition,
+						transform.position.y,
+						transform.position.z);
+				highestYPosition = worldObjectList[i].transform.position.x;
+			}
 		}
 	}
 	
